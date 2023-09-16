e3618e70f4a154478db81aff524af4c118aaa27c
Rishi Raj <rishiraj63229@gmail.com>
2023-09-16 23:43:56 +0530

    Initial commit

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args) {
+
         System.out.println("Hello!!!!!!!!!!!!");
+        System.out.println("Orwa nawa MONKEY D LUFFY ");
+        System.out.println("Kaizou kuoni orewa naruuuu");
     }
 }
